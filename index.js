2d3396c3466087bf68f537c3ef296a99fb174105
Jomagene <semjomagene@gmail.com>
2023-09-29 12:41:46 +0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log();
+function phrase (nom, verbe, compl){
+  let index1=0, index2=0, index3=0, result='';
+  index1=Math.floor((Math.random())*(nom.length));
+  index2=Math.floor((Math.random())*(verbe.length));
+  index3=Math.floor((Math.random())*(compl.length));
+  return result=(nom[index1]+' '+ verbe[index2]+' '+compl[index3])
+}
+console.log(phrase(etudiant, verbs, complement));
